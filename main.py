f52c7093f6188a66ba7c4e144b0e5758262dd820
gabyperez048 <155678647+gabyperez048@users.noreply.github.com>
2024-01-11 01:06:07 +0000

    Made code respond to input of which option the user chose
    Added booleans and aa while loop to determine the response depending on which corresponding number they chose.

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 def welcoming_user():
   print("Hello, this is Milo your digital assisstant!")
+  welcoming_user()
+  user_name, user_age = user_info()
 
 def user_info():
   name = input("What's your name?")
-  print("Hello " + name + "!" + " Nice to meet you!")"
+  print("Hello " + name + "!" + " Nice to meet you!")
   age = input(name + " how old are you?")
   return name, age
 
 def questions():
-  print("How can I ihelp you today" + name + "?")
+  print("How can I ihelp you today" + user_name + "?")
   print("1. Ask me a question")
   print("2. Provide information")
   print("3. End this coversation TT")
@@ -16,7 +18,7 @@ def questions():
   return choice 
 
 def ask_me_a_question():
-  response = input("Sure! What would you like to ask" + name +     "?")
+  input("Sure! What would you like to ask" + name +     "?")
   print("I'm sorry, I don't know the answer to that question.")
 
 def provide_information():
@@ -25,4 +27,19 @@ def provide_information():
 def end_this_conversation():
   print("Aw. I'm sorry to hear that" + name + "I hope you have a great day!")
         
-  
+def main():
+    
+
+    while True:
+        user_choice = questions()
+
+        if user_choice == '1':
+            ask_me_a_question()
+        elif user_choice == '2':
+            provide_information()
+        elif user_choice == '3':
+            end_this_conversation()
+        else:
+            print("Invalid choice. Please choose a valid option.")
+
+

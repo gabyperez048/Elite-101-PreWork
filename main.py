77af4b966033752e58028a6868092d8029d5773b
gabyperez048 <155678647+gabyperez048@users.noreply.github.com>
2024-01-11 01:28:09 +0000

    Did some final touches to make sure code was working how I wanted it to
    I fixed some bugs/errors and added and removed what was necessary.

--- main.py
+++ main.py
@@ -1,45 +1,55 @@
 def welcoming_user():
   print("Hello, this is Milo your digital assisstant!")
-  welcoming_user()
-  user_name, user_age = user_info()
 
 def user_info():
-  name = input("What's your name?")
-  print("Hello " + name + "!" + " Nice to meet you!")
-  age = input(name + " how old are you?")
-  return name, age
+  user_name = input("What's your name?")
+  print("Hello " + user_name + "!" + " Nice to meet you!")
+  age = input(user_name + " how old are you?")
+  print("Wow!")
+  return user_name, age
 
-def questions():
-  print("How can I ihelp you today" + user_name + "?")
+def questions(user_name):
+  
+  print("How can I help you today " + user_name + "?")
   print("1. Ask me a question")
   print("2. Provide information")
   print("3. End this coversation TT")
   choice = input("Please select an option (1/2/3) :D")
-  return choice 
+  return choice
 
-def ask_me_a_question():
-  input("Sure! What would you like to ask" + name +     "?")
-  print("I'm sorry, I don't know the answer to that question.")
+def ask_me_a_question(user_name):
+  question = input("Sure! What would you like to ask" + user_name +     "?")
+  print("I'm sorry, I don't know the answer to that" + question +".")
 
-def provide_information():
-  print("What information would you like to prorvide" + name +     "?")
+def provide_information(user_name):
+  input("What information would you like to prorvide" + user_name + "?")
 
-def end_this_conversation():
-  print("Aw. I'm sorry to hear that" + name + "I hope you have a great day!")
+def end_this_conversation(user_name):
+  print("Aw. I'm sorry to hear that" + user_name + "I hope you have a great day!")
         
 def main():
-    
 
-    while True:
-        user_choice = questions()
+  welcoming_user()
+  user_name, age = user_info()
+
+  while True:
+        user_choice = questions(user_name)
 
         if user_choice == '1':
-            ask_me_a_question()
+            ask_me_a_question(user_name)
+            
+          
         elif user_choice == '2':
-            provide_information()
+            provide_information(user_name)
+            
         elif user_choice == '3':
-            end_this_conversation()
+            end_this_conversation(user_name)
+            break
         else:
             print("Invalid choice. Please choose a valid option.")
 
+            questions(user_name)
+
+if __name__ == "__main__":
+  main()
 

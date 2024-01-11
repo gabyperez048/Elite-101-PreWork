747ee3ce11c4fd37d3ef949847d93d5a8216a9dd
gabyperez048 <155678647+gabyperez048@users.noreply.github.com>
2024-01-11 00:57:11 +0000

    Added respoonses to user's input
    Defined the function for the response to each option.

--- main.py
+++ main.py
@@ -14,4 +14,15 @@ def questions():
   print("3. End this coversation TT")
   choice = input("Please select an option (1/2/3) :D")
   return choice 
+
+def ask_me_a_question():
+  response = input("Sure! What would you like to ask" + name +     "?")
+  print("I'm sorry, I don't know the answer to that question.")
+
+def provide_information():
+  print("What information would you like to prorvide" + name +     "?")
+
+def end_this_conversation():
+  print("Aw. I'm sorry to hear that" + name + "I hope you have a great day!")
+        
   

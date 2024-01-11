89694d19ec6572386e4572d01442ee51c2b89242
gabyperez048 <155678647+gabyperez048@users.noreply.github.com>
2024-01-11 00:35:46 +0000

    Added welcoming and the questions for user info
    Defined the function for welcoming_user and user_info.

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('LALALALA$$$')
+def welcoming_user():
+  print("Hello, this is Milo your digital assisstant!")
+
+def user_info():
+  name = input("What's your name?")
+  print("Hello " + name + "!" + " Nice to meet you!")"
+  age = input(name + " how old are you?")
+  return name, age
+

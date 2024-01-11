9b77b97e48d887f138332a1ef241445246611d59
gabyperez048 <155678647+gabyperez048@users.noreply.github.com>
2024-01-11 00:49:17 +0000

    Created questions to ask chatbot
    Defined questions function that prints out different options for chatbot and added input for user to respond with choice

--- main.py
+++ main.py
@@ -7,3 +7,11 @@ def user_info():
   age = input(name + " how old are you?")
   return name, age
 
+def questions():
+  print("How can I ihelp you today" + name + "?")
+  print("1. Ask me a question")
+  print("2. Provide information")
+  print("3. End this coversation TT")
+  choice = input("Please select an option (1/2/3) :D")
+  return choice 
+  
